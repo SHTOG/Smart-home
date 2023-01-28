7bab855f09eedecd525a56b65b52414e4f0a5436
Roland <3051512493@qq.com>
2023-01-28 14:55:02 +0800

    "更新注释"

--- 基于STM32F4的zigbee终端_电灯/USER/main.c
+++ 基于STM32F4的zigbee终端_电灯/USER/main.c
@@ -9,7 +9,7 @@ int main(void) {
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
 	delay_init(168);    //初始化延时函数 
 	OLED_Init();
-	LED_Init();
+	LED_Init();			//初始化LEDio口
 	BEEP_Init();
 	Zigbee_Init(115200);
 	TIM3_PWM_Init(500-1,84-1);//84M/84=1Mhz的计数频率,重装载值500，所以PWM频率为 1M/500=2Khz.(周期为500us)
