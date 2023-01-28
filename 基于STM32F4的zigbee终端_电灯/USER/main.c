fca90d0da55237701f294e2c8af1cb0696c7e1f7
Roland <3051512493@qq.com>
2023-01-28 18:16:38 +0800

    "编码设置GBK"

--- 基于STM32F4的zigbee终端_电灯/USER/main.c
+++ 基于STM32F4的zigbee终端_电灯/USER/main.c
@@ -6,17 +6,17 @@
 #include "timer.h"
 
 int main(void) {
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
-	delay_init(168);    //初始化延时函数 
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//����ϵͳ�ж����ȼ�����2
+	delay_init(168);    //��ʼ����ʱ���� 
 	OLED_Init();
-	LED_Init();			//初始化LEDio口
-	BEEP_Init();		//初始化蜂鸣器（测试用）
+	LED_Init();			//��ʼ��LEDio��
+	BEEP_Init();		//��ʼ���������������ã�
 	Zigbee_Init(115200);
-	TIM3_PWM_Init(500-1,84-1);//84M/84=1Mhz的计数频率,重装载值500，所以PWM频率为 1M/500=2Khz.(周期为500us)
+	TIM3_PWM_Init(500-1,84-1);//84M/84=1Mhz�ļ���Ƶ��,��װ��ֵ500������PWMƵ��Ϊ 1M/500=2Khz.(����Ϊ500us)
 	while (1){
-		//电灯根据模式工作
+		//��Ƹ���ģʽ����
 		if(LEDmode == 0){
-			TIM_SetCompare1(TIM3,PWMval);	//修改比较值，修改占空比
+			TIM_SetCompare1(TIM3,PWMval);	//�޸ıȽ�ֵ���޸�ռ�ձ�
 		}
 		else if(LEDmode == 1){
 			if(PWMval == 500) direction = 0;
